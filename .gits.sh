cb7ea5f41753b1b29a9331036eeb64341717dede
Mark Phillips <me@compuglobalhypermegacorp.com>
2020-08-14 16:42:51 -0700

    initial commit

--- .gits.sh
+++ .gits.sh
@@ -2,3 +2,8 @@
 
 alias git-compu='git --git-dir=./.git-compu'
 alias git-github='git --git-dir=./.git-github'
+
+function gits () {
+  git --git-dir=./.git-compu "$@" && git --git-dir=./.git-github "$@"
+}
+
